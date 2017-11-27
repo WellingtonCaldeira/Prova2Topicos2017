4642020862c54567a402805c757e65fbb6b76edf
WellingtonCaldeira <wellingtoncaldeira.2018@gmail.com>
2017-11-27 21:05:50 -0200

    2 commit

--- shopping/shopping.sql
+++
@@ -1,7 +0,0 @@
-﻿create table shopping_order (
-  id integer not null,
-  description varchar(50) not null,
-  order_time timestamp not null,
-  constraint pk_order primary key (id)
-);
-create sequence seq_shopping_order;
